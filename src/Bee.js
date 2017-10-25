02c296fbc86f1624f8d5c12b73d1f9ecae6ab4f3
Katherine Fickel <katherine.fickel@gmail.com>
2017-10-24 21:40:37 -0700

    Completed Bee, Grub, and HoneyMakerBee

--- src/Bee.js
+++ src/Bee.js
@@ -1,3 +1,12 @@
 class Bee {
   // TODO..
+  constructor() {
+    this.age = 5;
+    this.color = 'yellow';
+    this.food = 'jelly';
+    this.job = 'Keep on growing';
+  }
+  eat() {
+    super.eat();
+  }
 };
