e7e979b178a4c9fae0d952e6430a6e79fdd0106c
Katherine Fickel <katherine.fickel@gmail.com>
2017-10-24 21:43:26 -0700

    Completed ForagerBee

--- src/ForagerBee.js
+++ src/ForagerBee.js
@@ -1,3 +1,15 @@
-class ForagerBee {
+class ForagerBee extends Grub {
   // TODO..
+  constructor(){
+    super();
+    this.age = 10;
+    this.job = 'find pollen';
+    this.color = 'yellow';
+    super.food;
+    this.canFly = true;
+    this.treasureChest = [];
+  }
+  forage(treasure) {
+    this.treasureChest.push(treasure);
+  }
 };
